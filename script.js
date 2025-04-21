c77c0758bfd4d928d76f335ad1e159849573c98e
brewmaster12 <seanywg@gmail.com>
2025-04-22 00:43:59 +0100

    Create initial Gameboard factory function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
- 
+function Gameboard() {
+    const board = ["","","","","","","","",""];
+
+    return {
+        move(place, player) {
+            board.splice(place, 1, player)
+        },
+        getBoard() {
+            return {board};
+        }
+    }
+};
+
+const turn = Gameboard();
+turn.move(2, "X");
+turn.move(4, "O");
+
+console.log(turn.getBoard());
