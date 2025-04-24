c15da81b83c40173451a6727d4c4a1df5d0316fc
brewmaster12 <seanywg@gmail.com>
2025-04-24 12:38:54 +0100

    Have initial functional console game

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
-const Gameboard = (function() {
-    const board = ["","","","","","","","",""];
+function Gameboard() {
+    const board = [];
 
-    return {
-        move(place, player) {
-            board.splice(place, 1, player)
-        },
-        getBoard() {
-            return {board};
-        }
+    for (i = 0; i < 9; i++) {
+        board.push("");
     }
-})();
 
-Gameboard.move(2, "X");
-Gameboard.move(4, "O");
+    const move = (place, player) =>  {
+        board.splice(place, 1, player)
+    };
 
-console.log(Gameboard.getBoard());
+    const getBoard = () => board;
+    
+    return {move, getBoard}
+};
+
+
+
+function gameController() {
+    const board = Gameboard();
+
+    const players = ["X", "O"];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.getBoard();
+        console.log(board.getBoard());
+    }
+
+    const takeTurn = (place) => {
+        board.move(place, getActivePlayer());
+
+        switchPlayerTurn();
+        printNewRound();
+    };
+
+    printNewRound();
+
+    return {takeTurn};
+};
+
+const game = gameController();
+
+/* console.log(game.takeTurn(4)); */
