81f7dc398878d0d501982f7cfdfc4ce92b1f1ede
brewmaster12 <seanywg@gmail.com>
2025-04-25 17:36:21 +0100

    Add win conditions

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ function Gameboard() {
     }
 
     const move = (place, player) =>  {
+        // Make sure place is not already taken
         if (!board[place]) {
-        board.splice(place, 1, player)
-        } else console.log("THAT PLACE IS ALREADY OCCUPIED");
+            // Place marker
+            board.splice(place, 1, player)
+        } else console.log("THAT PLACE IS ALREADY TAKEN");
     };
 
     const getBoard = () => board;
@@ -34,13 +36,32 @@ function gameController() {
         console.log(board.getBoard());
     }
 
+    const checkForWin = () => {
+        const win = board.getBoard();
+
+        const winningLines = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+            [0, 4, 8], [2, 4, 6]             // Diagonals
+        ];
+    
+        const hasWinner = winningLines.some(([a, b, c]) => 
+            win[a] && win[a] === win[b] && win[a] === win[c]
+        );
+    
+        if (hasWinner) console.log("WIN");
+    };
+
     const takeTurn = (place) => {
         board.move(place, getActivePlayer());
 
         switchPlayerTurn();
         printNewRound();
+        checkForWin();
     };
 
+    
+    // Initialise game
     printNewRound();
 
     return {takeTurn};
@@ -48,4 +69,4 @@ function gameController() {
 
 const game = gameController();
 
-/* console.log(game.takeTurn(4)); */
+/* console.log(game.takeTurn(3)); */
